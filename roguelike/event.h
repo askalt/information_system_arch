ea48d6d269010a917711276787e5839b6ec7426e
Albert Skalt <askalt@tarantool.org>
2024-12-21 23:25:19 +0300

    roguelike: add walls, chests, dungeons

--- roguelike/event.h
+++ roguelike/event.h
@@ -7,6 +7,12 @@ enum class EventType {
 };
 
 struct Event {
+  Event(SystemEvent sys_event)
+      : sys_event{std::move(sys_event)}, type{EventType::System} {}
+
+  Event(GameState::Event game_event)
+      : game_event(std::move(game_event)), type{EventType::Game} {}
+
   union {
     SystemEvent sys_event;
     GameState::Event game_event;
