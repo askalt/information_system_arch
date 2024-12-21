1dd3ff0ca5853809f569b7bc8d2ce79553fd6b29
Albert Skalt <askalt@tarantool.org>
2024-12-21 17:27:43 +0300

    roguelike: implement movement basics

--- roguelike/game_ui.h
+++ roguelike/game_ui.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <unordered_map>
+
 #include "curses.h"
 #include "event.h"
 #include "panic.h"
@@ -10,24 +12,75 @@
     panic(#f);       \
   }
 
+/* Console sizes. */
+const size_t W = 300;
+const size_t H = 200;
+
+/* Game field sizes. */
+const size_t W_FIELD = 150;
+const size_t H_FIELD = 70;
+
+void init_UI(int argc, char *argv[]) {
+#ifdef XCURSES
+  Xinitscr(argc, argv);
+#else
+  initscr();
+#endif
+  resize_term(H, W);
+}
+
+const std::unordered_map<IGameState::PartDescriptor, char>
+    PART_DESCRIPTOR_CHAR = {
+        {IGameState::PartDescriptor::PLAYER, 'p'},
+        {IGameState::PartDescriptor::WALL, '#'},
+};
+
 struct GameUI {
-  GameUI(const GameState &state) : state(state) {}
+  GameUI(std::shared_ptr<const IGameState> state) : state(std::move(state)) {}
 
   void draw() {
-    static int c = 0;
     safe_call(clear);
-    mvaddch(0, 0, ('A' + c) | A_BOLD);
-    refresh();
+    draw_border();
+    const auto objects = state->get_objects();
 
-    c = getch();
+    for (const auto &object : objects) {
+      const auto parts = object->get_layout();
+      for (const auto &part : parts) {
+        int x = part.x % (H_FIELD - 2) + 1;
+        int y = part.y % (W_FIELD - 2) + 1;
+        auto chr = PART_DESCRIPTOR_CHAR.find(part.descriptor);
+        assert(chr != PART_DESCRIPTOR_CHAR.end());
+        mvaddch(x, y, chr->second);
+      }
+    }
+
+    refresh();
   }
 
   Event next() {
-    return Event{.game_event = GameState::Event{
-                     .player_move = GameState::PlayerMoveEvent{},
-                     .type = GameState::EventType::PlayerMove,
-                 }};
+    getch();
+    return Event{.game_event =
+                     GameState::Event{
+                         .player_move = GameState::PlayerMoveEvent::Right,
+                         .type = GameState::EventType::PlayerMove,
+                     },
+                 .type = EventType::Game};
   }
 
-  const GameState &state;
+ private:
+  void draw_border() {
+    mvaddch(0, 0, '*');
+    for (size_t i = 0; i < W_FIELD - 2; ++i) addch('-');
+    addch('*');
+    for (size_t i = 1; i < H_FIELD - 1; ++i) {
+      mvaddch(i, 0, '|');
+      mvaddch(i, W_FIELD - 1, '|');
+    }
+    mvaddch(H_FIELD - 1, 0, '*');
+    for (size_t i = 0; i < W_FIELD - 2; ++i) addch('-');
+    addch('*');
+    refresh();
+  }
+
+  std::shared_ptr<const IGameState> state;
 };
