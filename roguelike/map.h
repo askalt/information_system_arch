ff9df1688e137d08b7e20f5d23016f27b762df91
Albert Skalt <askalt@tarantool.org>
2024-12-22 22:07:01 +0300

    roguelike: add ogr

--- roguelike/map.h
+++ roguelike/map.h
@@ -1,19 +1,46 @@
 #pragma once
+#include <algorithm>
 #include <filesystem>
 #include <vector>
 
 #include "objects.h"
+#include "panic.h"
 #include "state.h"
 
 struct Map {
   friend class GameState;
   friend class Player;
+  friend class Mob;
 
   /* Each map contains player. */
   Map(IGameState::Object* player);
   Map();
   Map(const std::filesystem::path& path);
   friend class World;
+
+  bool has_object(int x, int y, const IGameState::Object* exclude) const;
+
+  template <typename T>
+  bool remove_object(std::vector<std::unique_ptr<T>>& container, T* item) {
+    size_t pos = std::string::npos;
+    for (size_t i = 0; i < container.size(); ++i) {
+      if (container[i].get() == item) {
+        pos = i;
+        break;
+      }
+    }
+    if (pos != std::string::npos) {
+      container.erase(container.begin() + pos);
+      auto as_obj = static_cast<IGameState::Object*>(item);
+      auto it = std::find(objects.begin(), objects.end(), as_obj);
+      if (it == objects.end()) {
+        panic("there must be object");
+      }
+      objects.erase(it);
+      return true;
+    }
+    return false;
+  }
 
  private:
   std::vector<std::unique_ptr<Enter>> enters;
